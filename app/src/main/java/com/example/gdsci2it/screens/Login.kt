c45c204ff1e657b7faa1da5a4ae09ef62c032bd5
avishkarp23 <avishkarp_t20031@students.isquareit.edu.in>
2022-10-14 19:07:19 +0530

    Onboarding and Login screen added

---
+++ app/src/main/java/com/example/gdsci2it/screens/Login.kt
@@ -0,0 +1,152 @@
+package com.example.gdsci2it.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.gdsci2it.R
+
+@Composable
+fun Login(){
+    var username by remember{
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(painter = painterResource(id = R.drawable.istockphoto_1001279264_612x612) , contentDescription ="gdsc_logo",modifier= Modifier
+            .fillMaxSize()
+            , contentScale = ContentScale.Crop)
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(28.dp)
+            .alpha(0.3f)
+            .clip(CutCornerShape(15.dp))
+            .background(MaterialTheme.colors.background)) {
+
+        }
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(48.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+            Spacer(modifier = Modifier.height(55.dp))
+            LoginHeader()
+            Spacer(modifier = Modifier.height(55.dp))
+            LoginField(username,
+            password,
+            onpasswordchange = {
+                password=it
+            },
+            onusernamechange = {
+                username=it
+            },
+            onforgotpasswordclick = {
+
+            })
+            Spacer(modifier = Modifier.height(40.dp))
+            LoginFooter( onsigninClick = {
+
+            },
+            onSignupClick = {
+
+            })
+        }
+
+
+    }
+}
+
+@Composable
+fun LoginHeader(){
+
+        Text(text = "Welcome Back", fontSize = 36.sp, fontWeight = FontWeight.ExtraBold)
+         Spacer(modifier = Modifier.height(8.dp))
+        Text(text = "Sign in to Continue", fontSize = 18.sp, fontWeight = FontWeight.SemiBold)
+
+    
+
+}
+@Composable
+fun ColumnScope.LoginField(username:String,password:String,onusernamechange: (String) ->Unit,onpasswordchange: (String)->Unit,onforgotpasswordclick: ()->Unit){
+demofield(value = username,
+    Onvaluechange =onusernamechange ,
+    label = "Username",
+    placeholder ="Enter Your Email Id",
+leadingicon = {Icon(imageVector = Icons.Default.Email, contentDescription ="Email" )},
+        keyboardoption = KeyboardOptions(keyboardType = KeyboardType.Email, imeAction = ImeAction.Next)
+    
+)
+    Spacer(modifier = Modifier.height(8.dp))
+    demofield(value = password,
+        Onvaluechange = onpasswordchange,
+        label ="Password" ,
+        placeholder = "Enter Password",
+        VisualTransform = PasswordVisualTransformation(),
+        leadingicon = { Icon(imageVector = Icons.Default.Lock, contentDescription ="Password" )},
+       keyboardoption = KeyboardOptions(keyboardType = KeyboardType.Password, imeAction = ImeAction.Go)
+    )
+    
+    TextButton(onClick = onforgotpasswordclick, modifier = Modifier.align(Alignment.End)) {
+        Text(text = "Forgot Password?", color = Color.Black )
+    }
+}
+@Composable
+fun LoginFooter(
+    onsigninClick: ()->Unit,
+    onSignupClick: ()->Unit
+){
+
+
+Button(onClick = onsigninClick, modifier = Modifier.fillMaxWidth(), shape = MaterialTheme.shapes.medium) {
+    Text(text = "Sign In")
+}
+TextButton(onClick = onSignupClick) {
+    Text(text = "Don't Have an account?,click here")
+}
+}
+
+@Composable
+fun demofield(value:String,Onvaluechange:(String)->Unit,label:String,placeholder:String,VisualTransform:VisualTransformation= VisualTransformation.None,keyboardoption:KeyboardOptions=KeyboardOptions.Default,leadingicon:@Composable (() -> Unit)?=null){
+    OutlinedTextField(value = value,
+        onValueChange =Onvaluechange,
+        label = {
+            Text(text = label)
+        },
+        placeholder = {
+            Text(text = placeholder)
+        },
+        visualTransformation = VisualTransform,
+        keyboardOptions =keyboardoption,
+        leadingIcon = leadingicon
+    
+    )
+}
+@Preview
+@Composable
+fun preview(){
+    Login()
+}
---
+++ app/src/main/java/com/example/gdsci2it/screens/Onboarding.kt
@@ -0,0 +1,59 @@
+package com.example.gdsci2it.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.gdsci2it.R
+@Composable
+fun Onboarding() {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.SpaceAround) {
+        Image(painter = painterResource(id =R.drawable.gdsc3
+        ), contentDescription =null , modifier = Modifier.padding(top=85.dp)
+            .size(240.dp)
+            .clip(
+                CircleShape
+            ), contentScale = ContentScale.FillBounds)
+        Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "For the Students", fontWeight = FontWeight.ExtraBold, fontSize = 26.sp)
+            Text(text = "By the Students", fontWeight = FontWeight.ExtraBold, fontSize = 24.sp, color = MaterialTheme.colors.primary)
+        }
+
+        Text(text = "One stop resource for students' need", fontSize = 20.sp,
+            modifier = Modifier.padding(horizontal = 6.dp), fontStyle = FontStyle.Italic, textAlign = TextAlign.Center)
+        IconButton(onClick = { }, modifier = Modifier
+            .clip(CircleShape)
+            .background(
+                MaterialTheme.colors.primary
+            )) {
+            Icon(Icons.Default.ArrowForward, contentDescription =null,tint= MaterialTheme.colors.onPrimary )
+
+        }
+    }
+}
+/*@Preview(showBackground = true)
+@Composable
+fun preview( ){
+    Onboarding()
+}*/
